8d46b8744ae458fdc628afc0ed57b856151bbd6c
ClaudiaRoshini-07 <79586088+ClaudiaRoshini-07@users.noreply.github.com>
2021-06-15 14:14:57 +0530

    Day4.py

---
+++ Day4.py
@@ -0,0 +1,39 @@
+1)Create three variables (a,b,c) to same value of any integer & do the following
+a) Divide a by 10
+b) Multiply b by 50
+c) Add c value by 60
+
+>>> a = b = c =100
+>>> print("value of  a=",a,"Value of b=",b,"Value of c=",c)
+value of  a= 100 Value of b= 100 Value of c= 100
+
+>>> print("Divide a by 10=",a/10)
+Divide a by 10= 10.0
+
+>>> print("Multiply b by 50=",b*50)
+Multiply b by 50= 5000
+
+>>> print("Add c by 50=",c+60)
+Add c by 50= 160
+
+2)Create a String variable of 5 characters and replace the 3rd character with G
+
+>>> Str_var="abcdj"
+>>> print(Str_var)
+abcdj
+>>> print("After replacing G",Str_var.replace("c","G"))
+After replacing G abGdj
+
+>>> print("After replacing G",Str_var[:2]+'G'+Str_var[3:])
+After replacing G abGdj
+
+3)Create two values (a,b) of int,float data type & convert the vise versa, Hint : convert a from int to float datatype & b from float to int datatype
+
+>>>a=2
+>>> b=20.5
+>>> print("a=",a,"b=",b)
+a= 2 b= 20.5
+>>> print("After changing a to float",float(a))
+After changing a to float 2.0
+>>> print("After changing b to int",int(b))
+After changing b to int 20
