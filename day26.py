e4b9a45c6728348431443eb59bc1aa7f481f77ac
ClaudiaRoshini-07 <79586088+ClaudiaRoshini-07@users.noreply.github.com>
2021-07-11 10:29:36 +0530

    day26.py

---
+++ day26.py
@@ -0,0 +1,16 @@
+//•	Enumerate a python list and try to print the counter with the list value
+
+l1= ['Tina','Jack','John','Mojo','Kavi','karthik']
+print(list(enumerate(l1)))
+
+OUTPUT:
+[(0, 'Tina'), (1, 'Jack'), (2, 'John'), (3, 'Mojo'), (4, 'Kavi'), (5, 'karthik')]
+
+
+//•	Enumerate a python tuple and try to print the counter with the tuple value
+
+l1= ('Tina','Jack','John','Mojo','Kavi','karthik')
+print(tuple(enumerate(l1)))
+
+OUTPUT:
+((0, 'Tina'), (1, 'Jack'), (2, 'John'), (3, 'Mojo'), (4, 'Kavi'), (5, 'karthik'))
